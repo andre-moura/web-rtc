23f3443aa373c2afdbc88e38521671292222cc2b
andre-moura <61800450+andre-moura@users.noreply.github.com>
2023-06-06 21:14:20 -0300

    Adding register form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,20 +2,22 @@ import React, { useState } from 'react';
 import Button from '../components/Button';
 import Input from '../components/Input';
 
-interface LoginForm {
+interface RegisterForm {
+    email: string,
     username: string;
     password: string;
 }
 
-const Login: React.FC = () => {
-    const [loginForm, setLoginForm] = useState<LoginForm>({
+const Register: React.FC = () => {
+    const [registerForm, setRegisterForm] = useState<RegisterForm>({
+        email: '',
         username: '',
         password: '',
     });
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
-        setLoginForm((prevState) => ({
+        setRegisterForm((prevState) => ({
             ...prevState,
             [name]: value,
         }));
@@ -23,19 +25,27 @@ const Login: React.FC = () => {
 
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault();
-        // Perform login logic using loginForm.username and loginForm.password
+        // Perform login logic using register Form.username and register Form.password
     };
 
     return (
         <>
-            <h2>Login</h2>
+            <h2>Register</h2>
             <form onSubmit={handleSubmit}>
+                <Input
+                    label="E-mail"
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={registerForm.email}
+                    onChange={handleInputChange}
+                />
                 <Input
                     label="Username"
                     type="text"
                     id="username"
                     name="username"
-                    value={loginForm.username}
+                    value={registerForm.username}
                     onChange={handleInputChange}
                 />
                 <Input
@@ -43,13 +53,13 @@ const Login: React.FC = () => {
                     type="password"
                     id="password"
                     name="password"
-                    value={loginForm.password}
+                    value={registerForm.password}
                     onChange={handleInputChange}
                 />
-                <Button type="submit">Login</Button>
+                <Button type="submit">Register</Button>
             </form>
         </>
     );
 };
 
-export default Login;
+export default Register;
