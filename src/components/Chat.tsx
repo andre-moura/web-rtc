4c5622f01eb5fa32c12140efee745fa12060495c
andre-moura <61800450+andre-moura@users.noreply.github.com>
2023-06-17 15:13:50 -0300

    style: adding icons to chat component
    Adding call, video call and add friend icon

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FaPhone, FaVideo, FaUserPlus } from 'react-icons/fa';
 import '../assets/css/stylesheet.css';
 
 interface ChatProps {
@@ -20,6 +21,21 @@ interface Message {
 const Chat: React.FC<ChatProps> = ({ conversationId }) => {
   const [inputValue, setInputValue] = useState('');
 
+  const handlePhoneClick = () => {
+    // Handle phone icon click
+    console.log('Phone icon clicked');
+  };
+
+  const handleVideoClick = () => {
+    // Handle video icon click
+    console.log('Video icon clicked');
+  };
+
+  const handleUserPlusClick = () => {
+    // Handle user plus icon click
+    console.log('User plus icon clicked');
+  };
+
   const handleMessageSend = () => {
     if (inputValue.trim() === '') {
       return;
@@ -37,7 +53,7 @@ const Chat: React.FC<ChatProps> = ({ conversationId }) => {
       { id: '1', sender: 'User', content: 'Hello' },
       { id: '2', sender: 'Friend 1', content: 'Hi' },
       // Add more messages as needed
-    ]
+    ],
   };
 
   if (!conversationId || conversationId !== conversation.id) {
@@ -47,10 +63,17 @@ const Chat: React.FC<ChatProps> = ({ conversationId }) => {
   return (
     <div className="chat">
       <div className="chat-header">
-        <h3>{conversation.name}</h3>
+        <h3>
+          {conversation.name}
+        </h3>
+          <div className="icon-container">
+            <FaPhone className="icon" onClick={handlePhoneClick} />
+            <FaVideo className="icon" onClick={handleVideoClick} />
+            <FaUserPlus className="icon" onClick={handleUserPlusClick} />
+          </div>
       </div>
       <div className="chat-messages">
-        {conversation.messages.map(message => (
+        {conversation.messages.map((message) => (
           <div className="message" key={message.id}>
             <div className="message-sender">{message.sender}</div>
             <div className="message-content">{message.content}</div>
@@ -61,7 +84,7 @@ const Chat: React.FC<ChatProps> = ({ conversationId }) => {
         <input
           type="text"
           value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
+          onChange={(e) => setInputValue(e.target.value)}
           placeholder="Type a message..."
         />
         <button onClick={handleMessageSend}>Send</button>
